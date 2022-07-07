91b6e9428c0a11567587ffcae7027c0becf84098
Alberto Gimeno <gimenete@gmail.com>
2022-07-07 11:43:24 +0200

    Add README and comments

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# serializer
+# Serializer
+
+A parser/serializer that supports serializing many JS built-in types that JSON.parse doesn't support: `Set`, `Map`, `BigInt`, `Date`.
+
+It also supports serializing arbitrary objects and parsing them into their original classes. This requires to use the `serializer.add(Class)` method.
+
+Example:
+
+```js
+class Person {
+  constructor(dateOfBirth) {
+    this.dateOfBirth = dateOfBirth
+  }
+
+  age() {
+    // Simplified implementation
+    return new Date().getFullYear() - this.dateOfBirth.getFullYear()
+  }
+}
+
+const serializer = new Serializer()
+serializer.add(Person)
+
+const original = new Person(new Date('1984-06-16'))
+
+const json = serializer.toJSON(original)
+const person = serializer.fromJSON(json)
+console.log('age', person.age())
+```
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// These will be used to set special fields to serialized objects.
+// So objects should not use this field names. I think that's fine. Another approach would be to generate two objects:
+// One containing the "data tree" and another containing the a tree with the type information.
 const META = '__'
 const VALUE = '___'
 
@@ -9,6 +12,10 @@ class Serializer {
   }
 
   toJSON(data) {
+    // We are not using JSON.stringify() and the replacer function here, because the replacer receives
+    // the result of toJSON() if the object has a toJSON() method. This is a problem for the Date type:
+    // we get a string, because Date.toJSON() returns the date formatted into a ISO string alreayd,
+    // so it's not possible to guess the type of the original object.
     function visit(obj) {
       if (Array.isArray(obj)) return obj.map(visit)
 
@@ -42,6 +49,8 @@ class Serializer {
         if (clazz === 'Set') return new Set(val)
         if (clazz === 'Map') return new Map(Object.entries(val))
         if (clazz === 'bigint') return BigInt(val)
+        // TODO: this is ignored. In order to support undefined we'd need to traverse the JSON tree ourselves.
+        // Instead of using the JSON.parse() reviver argument.
         if (clazz === 'undefined') return undefined
         return rest
       }
